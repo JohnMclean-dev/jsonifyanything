f13e76326cc0086058e8dcb6806d1cf55b1e8f2b
JohnMcLean-dev <john1.mclean@outlook.com>
2025-03-10 21:54:12 -0400

    gotta re-think this whole thing, lmao

--- jsonifyanything.py
+++ jsonifyanything.py
@@ -1,18 +1,67 @@
 #!/usr/bin/env python3
+from collections.abc import Iterable
 import json
 
+win='Data written'
+fail='Data not written.'
+
+def is_json_serializable(obj):
+    try:
+        json.dumps(obj)
+        return True
+    except (TypeError, OverflowError):
+        return False
+
+def clean_data(data):
+
+    if isinstance(data, Iterable):
+        clean_data(data)
+    
+    elif is_json_serializable(data):
+        return data
+    
+    else:
+        new_data=f''' Could not convert data below.
+            
+            {str(data)}
+        
+        '''
+        type(new_data)
+        print(new_data)
+        return new_data
+
 def write_json(data):
-    with open("data.json", "w") as json_file:
-        json.dump(data, json_file, indent=4)
+    try:
+        with open("data.json", "w") as json_file:
+            json.dump(data, json_file, indent=4)
 
-
+    except Exception as e:
+        clean_data(data)
+        
 def main(*args, **kwargs):
-    return (args, kwargs)
+    if args and kwargs:
+        # TODO: make decision
+        write_json((args, kwargs))
+        return win
+
+    elif args:
+        # TODO: make decision
+        return fail
+
+    elif kwargs:
+        # TODO: make decision
+        return fail
+
+    else:
+        return win
+
 
 if __name__ == "__main__":
     try:
-        json_response = main(123, 5, j="g")
-        write_json(json_response)
+        # din=pd.read_csv('data.csv')
+        # json_response = main(din, 123, 5, j="g")
+        var=set()
+        json_response=main(var, v=var)
         print(json_response)
 
     except Exception as e:
